75ea7e483075b0cbf8c3749de7c70c0a4f335fa3
Lukas Pistrol <lukas@pistrol.com>
2022-12-29 16:25:46 +0100

    skip package plugin validation on docc build

--- .github/scripts/build-docc.sh
+++ .github/scripts/build-docc.sh
@@ -4,4 +4,5 @@ export LC_ALL=en_US.UTF-8
 
 set -o pipefail && xcodebuild clean docbuild -scheme LPMapView \
     -destination generic/platform=iOS \
+    -skipPackagePluginValidation \
     OTHER_DOCC_FLAGS="--transform-for-static-hosting --hosting-base-path LPMapView --output-path ./docs" | xcpretty
