4fc903fc33cd5f6e2ebf94905ab01bd413459786
Lukas Pistrol <lukas@pistrol.com>
2022-12-29 16:16:13 +0100

    add gh actions

---
+++ .github/scripts/build-docc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export LC_ALL=en_US.UTF-8
+
+set -o pipefail && xcodebuild clean docbuild -scheme LPMapView \
+    -destination generic/platform=iOS \
+    OTHER_DOCC_FLAGS="--transform-for-static-hosting --hosting-base-path LPMapView --output-path ./docs" | xcpretty
