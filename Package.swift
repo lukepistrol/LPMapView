87fcd5f51f42daa0bea84386ed300946452ba8f7
Lukas Pistrol <lukas@pistrol.com>
2023-01-02 17:07:58 +0100

    remove swiftlint plugin

--- Package.swift
+++ Package.swift
@@ -13,16 +13,10 @@ let package = Package(
             targets: ["LPMapView"]
         )
     ],
-    dependencies: [
-        .package(url: "https://github.com/lukepistrol/SwiftLintPlugin", from: "0.2.2")
-    ],
     targets: [
         .target(
             name: "LPMapView",
-            dependencies: [],
-            plugins: [
-                .plugin(name: "SwiftLint", package: "SwiftLintPlugin")
-            ]
+            dependencies: []
         )
     ]
 )
